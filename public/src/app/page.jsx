0ae2b5609c49e5f8c6a03637f1aa000a08a72941
Lakshya Singh <lakshyac99@gmail.com>
2024-08-11 21:31:24 +0530

    project setup everything is working fine

--- public/src/app/page.jsx
+++ public/src/app/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
+import Navbar from "../components/navbar/Navbar";
+
 const page = () => {
-  return <div>page</div>;
+  return <div>
+    <Navbar />
+    </div>;
 };
 
 export default page;
--- public/src/components/navbar/Navbar.jsx
+++ public/src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import AirBnbLogo from "../../svg/airbnb-logo";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <header className="w-full flex-col justify-center transition-all duration-300">
+    <div className="flex items-center justify-between px-20">
+      <div className="flex-grow basis-0">
+        <div className="w-max cursor-pointer">
+          <AirBnbLogo />
+        </div>
+      </div>
+    </div>
+  </header>;
 };
 
 export default Navbar;
