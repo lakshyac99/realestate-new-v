41cb2fd51c1f42582c22a63182e936b223f6df48
Lakshya Singh <lakshyac99@gmail.com>
2024-08-12 15:47:49 +0530

    updated navbar

--- public/src/components/footer/Footer.jsx
+++ public/src/components/footer/Footer.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+
+  const links = [
+    "privacy", 
+    "terms", 
+    "sitemap", 
+    "company details", 
+    "destinations"
+  ];
+
+  return <div className="px-20 border-t border-t-gray-200 py-4 flex justify-between w-full text-sm z-50 bg-white items-center">Footer</div>;
 }
--- public/src/components/navbar/Navbar.jsx
+++ public/src/components/navbar/Navbar.jsx
@@ -1,16 +1,41 @@
-import React from "react";
 import AirBnbLogo from "../../svg/airbnb-logo";
+import Image from "next/image";
+import React from "react";
+import { FiGlobe } from 'react-icons/fi';
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
-  return <header className="w-full flex-col justify-center transition-all duration-300">
-    <div className="flex items-center justify-between px-20">
-      <div className="flex-grow basis-0">
-        <div className="w-max cursor-pointer">
-          <AirBnbLogo />
+  return (
+    <header className="w-full flex-col justify-center transition-all duration-300 border-b border-b-gray-200" >
+      <div className="flex items-center justify-between px-20 h-20">
+        <div className="flex-grow basis-0">
+          <div className="w-max cursor-pointer">
+            <AirBnbLogo />
+          </div>
+        </div>
+        <div className="flex-grow basis-0">
+          <ul className="flex items-center justify-end gap-6 font-medium">
+            <li className="cursor-pointer">
+              <span>Airbnb your home</span>
+            </li>
+            <li className="cursor-pointer">
+              <FiGlobe />
+            </li>
+            <li className="flex cursor-pointer items-center gap-2 border border-gray-300 py-2 px-3 rounded-full hover:shadow-xl transition-all duration-50">
+              <RxHamburgerMenu />
+              <span>
+                <Image
+                  src="/empty-profile.png"
+                  alt="profile"
+                  height={30}
+                  width={30} />
+              </span>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
-  </header>;
+    </header >
+  );
 };
 
 export default Navbar;
