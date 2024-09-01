ad2d530dbfb33dd55d707a09eb0b0fc1d2b92c14
Lakshya Singh <lakshyac99@gmail.com>
2024-09-01 10:03:21 +0530

    Improve the login ui, opacity

--- public/src/components/auth/AuthModal.jsx
+++ public/src/components/auth/AuthModal.jsx
@@ -39,11 +39,11 @@ const AuthModal = () => {
 
   return (
     <div className="relative z-50">
-      <div className="fixed inset-0 bg-gray-500 opacity-75 transition-opacity">
-        <div className="fixed inset-0 z-10 overflow-y-auto">
+      <div className="fixed inset-0 bg-gray-500 opacity-95 transition-opacity">
+        <div className="fixed inset-0 z-10 overflow-y-auto ">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-              <div className="bg-white pb-4 pt-5">
+              <div className="bg-white pb-4 pt-5 ">
                 <div className="border-b border-b-gray-200 flex items-center justify-center relative pb-5">
                   <span
                     className="absolute left-5 cursor-pointer text-lg"
